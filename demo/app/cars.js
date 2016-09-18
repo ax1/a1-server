802c83597d1f49f8aeb6180cee7f7ced97d54d4f
Angel Rego <angelrego@gmail.com>
2016-09-18 13:51:05 +0200

    demo:REST: cars service example by using async/await instead of callbacks
    This is a deo on how to create REST services.
    
    The new format has shinked the code by 2X, so the improvement on reading/writing REST services is HUGE.
    
    This also demonstrates that the risk of changing from callbacks to async/await format is clearly justified later when developing web applications.

--- demo/app/cars.js
+++ demo/app/cars.js
@@ -1,71 +1,36 @@
-let db=require('../../lib/persistence').db()
+/**
+ * REST example
+ */
+
+let db=require('../../lib/persistence').getDB()
 
 module.exports={
-  get,
-  post,
-  put,
-  delete:remove
+  get, post, put, delete:remove
 }
 
-function get(request,response,params,callback){
-  try{
-    if (params && Object.keys(params).length>0){
-      db.find({_id:params.id},callback)
-    }else{
-      db.find({},callback)
-    }
-  }catch (e){
-    callback(e)
-  }
+async function get(request,response,params){
+  if (params && Object.keys(params).length>0) return await db.findAsync({_id:params.id})
+  else return await db.findAsync({})
 }
 
-function post(request,response,params,callback){
-  try{
-    db.insert(JSON.parse(request.body),(err,data)=>{
-      if (err) callback(err)
-      else{
-        response.statusCode=201
-        let location=request.url
-        if (location.endsWith('/')) location=location+data._id
-        else location=location+'/'+data._id
-        response.setHeader('Location',location)
-        callback(null,{'_id':data._id})
-      }
-    })
-  }catch (e){
-    callback(e)
-  }
+async function post(request,response,params){
+  let data=await db.insertAsync(JSON.parse(request.body))
+  response.statusCode=201
+  let location=request.url
+  if (location.endsWith('/')) location=location+data._id
+  else location=location+'/'+data._id
+  response.setHeader('Location',location)
+  return {'_id':data._id}
 }
 
-function put(request,response,params,callback){
-  try{
-    db.update({_id:params.id},JSON.parse(request.body),{},(err, numAffected, affectedDocuments, upsert)=>{
-      if(err){
-        callback(err)
-      }else{
-        if(numAffected===0){
-          response.statusCode=404
-          callback(404)
-        }else{
-          response.statusCode=204
-          callback(null,'')
-        }
-      }
-    })
-  }catch (e){
-    callback(e)
-  }
+async function put(request,response,params){
+  let numAffected=await db.updateAsync({_id:params.id},JSON.parse(request.body),{})
+  if(numAffected===0)response.statusCode=404
+  else response.statusCode=204
+  return numAffected
 }
 
-function remove(request,response,params,callback){
-  try{
-    if(params && Object.keys(params).length){
-      db.remove({_id:params.id},{},callback)
-    }else{
-      //normal http delete doesn't contain body (so if no id, delete all)
-      db.remove({},{multi:true},callback)
-    }
-  }catch (e){
-    callback(e)
-  }
+async function remove(request,response,params){
+  if(params && Object.keys(params).length)  return await db.removeAsync({_id:params.id},{})
+  else return await db.remove({},{multi:true}) //normal http delete doesn't contain body (so if no id, delete all)
 }
