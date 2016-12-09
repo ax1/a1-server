84d70724ad2730dd4b86243095c918914becb4ea
Angel Rego <angelrego@gmail.com>
2016-12-09 23:21:20 +0100

    development: check easiest cluster and think abobut adapting to server
    In the end, the best is to leave users to use or not the cluster, if needed they can perform server.start in each process.
    
    Best for production is still pm2 or similar packages.

--- demo/sandbox.js
+++ demo/sandbox.js
@@ -1,37 +1,17 @@
-const cluster = require('cluster');
-const http = require('http');
-const numCPUs = require('os').cpus().length;
+const cluster = require('cluster')
+const http = require('http')
+const numCPUs = require('os').cpus().length
 
 if (cluster.isMaster) {
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
+  for (var i = 0; i < numCPUs; i++) {cluster.fork()}
   cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
+    console.log(`worker ${worker.process.pid} died`)
     cluster.fork();
-  });
+  })
 } else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
   http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end(process);
-  }).listen(8080);
-}
-
-// http.createServer((req, res) => {
-//   res.writeHead(200);
-//   res.end(process());
-// }).listen(8080);
-
-const text=`aaaaaaaaaaaaa`
-
-function process(){
-  let res=text
-  while(res.length>0){
-    res=res.replace(res.substring(0,1),'')
-  }
-  return res
+    res.writeHead(200)
+    res.write('hello world')
+    res.end()
+  }).listen(8080)
 }
