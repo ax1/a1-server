d1053b6ed345b7ad338f38f9d56d61ec040ad2a3
Angel Rego <angelrego@gmail.com>
2017-03-04 17:13:18 +0100

    demo: better REST service cars/:id implementation

--- demo/app/cars.js
+++ demo/app/cars.js
@@ -2,48 +2,48 @@
  * REST example
  */
 
-const db=[
-  {name:"toyota",engine:"2222",id:"doKnnvbRCpt7fQaC"},
-  {name:"toyota",engine:"1111",id:"tibP9rhyyu3dzTtX"}
-]
+const db={
+  "doKnnvbRCpt7fQaC":{name:"toyota",engine:"2222",id:"doKnnvbRCpt7fQaC"},
+  "tibP9rhyyu3dzTtX":{name:"toyota",engine:"1111",id:"tibP9rhyyu3dzTtX"}
+}
 
 module.exports={
   get, post, put, delete:remove
 }
 
 async function get(request,response,params){
-  if (params && Object.keys(params).length>0) return  db.find(item=>item.id==params.id)
+  if (params && Object.keys(params).length>0) return  db[params.id]
   else return db
 }
 
 async function post(request,response,params){
   const data=JSON.parse(request.body)
-  db.push(data)
+  if(!data.id) data.id=new Date().valueOf()
+  db[data.id]=data
   response.statusCode=201
   let location=request.url
   if (location.endsWith('/')) location=location+data.id
   else location=location+'/'+data.id
   response.setHeader('Location',location)
-  return {'id':data.id}
+  return data
 }
 
 async function put(request,response,params){
-  const index= db.findIndex(el=>el.id===params.id)
-  if (index>-1){
-    db.splice(index,1,JSON.parse(request.body))
+  const data=JSON.parse(request.body)
+  if(db[data.id]){
+    db[data.id]=data
     response.statusCode=204
+    return data
   }else{
     throw(404)
   }
 }
 
 async function remove(request,response,params){
-  if(params && Object.keys(params).length){
-    const index=db.findIndex(item=>item.id==params.id)
-    if (index>-1) {
-      db.splice(index,1)
-      return 1
-    }else return 0
+  if(params){
+    if (db[params.id]){delete db[params.id];return 1}
+    else throw (404)
+  }else{
+    throw(400)
   }
-  else throw(404)
 }
